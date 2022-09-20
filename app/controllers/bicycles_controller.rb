6e68694535e8f597cb1b3f17e23ae108bd67858b
Samuel Paget <sjapaget@gmail.com>
2022-09-20 15:16:01 +0200

    Add bicycles#new & bicycles#create actions

--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -2,4 +2,27 @@ class BicyclesController < ApplicationController
   def index
     @bicycles = policy_scope(Bicycle)
   end
+
+  def new
+    @bicycle = Bicycle.new
+    authorize @bicycle
+  end
+
+  def create
+    @bicycle = Bicycle.new(bicycle_params)
+    @bicycle.user = current_user
+    @bicycle.photo = Bicycle::DEFAULT_IMG unless params[:photo].present?
+    if @bicycle.save
+      redirect_to bicycles_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+    authorize @bicycle
+  end
+
+  private
+
+  def bicycle_params
+    params.require(:bicycle).permit(:title, :description, :category, :size, :price_per_day, :photo)
+  end
 end
--- app/policies/bicycle_policy.rb
+++ app/policies/bicycle_policy.rb
@@ -5,4 +5,12 @@ class BicyclePolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def new?
+    create?
+  end
+
+  def create?
+    true
+  end
 end
