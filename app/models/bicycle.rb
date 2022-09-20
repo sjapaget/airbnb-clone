ae66a7eeaa9fc36b3975a311bf8fa7419beb3328
Samuel Paget <sjapaget@gmail.com>
2022-09-20 11:19:16 +0200

    Refactor for style

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -3,7 +3,9 @@ class Bicycle < ApplicationRecord
 
   validates :title, :category, :description, :price, :user, presence: true
   validates :price, numericality: { only_integer: true, greater_than: 0 }
-  validates :size, inclusion: { in: %w[small medium large], message: "Sorry, %{value} is not a size we accept" }
-  validates :category, inclusion: { in: %w[road gravel fixie child mountain city other], message: "Sorry, %{value} is not a size we accept" }
+  validates :size, inclusion: { in: %w[small medium large],
+                                message: "Sorry, %<value> is not a size we accept" }
+  validates :category, inclusion: { in: %w[road gravel fixie child mountain city other],
+                                    message: "Sorry, %<value> is not a category we accept" }
   validates :title, :description, length: { minimum: 1 }
 end
